7bda35a05fc9949ff28ad354d4ad38871163c876
Luis Vienrich <a20172501@pucp.edu.pe>
2023-01-20 23:38:43 -0500

    Updating Connect4

---
+++ Connect4/Connect4/Connect4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Luis Vienrich on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
