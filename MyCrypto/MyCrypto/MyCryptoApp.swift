11b109a5eb90dff3d28dacce0e04f11ea2a0a796
Luis Vienrich <a20172501@pucp.edu.pe>
2023-02-15 22:01:06 -0500

    Create MyCrypto v1.0

---
+++ MyCrypto/MyCrypto/MyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCryptoApp.swift
+//  MyCrypto
+//
+//  Created by Luis Vienrich on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
