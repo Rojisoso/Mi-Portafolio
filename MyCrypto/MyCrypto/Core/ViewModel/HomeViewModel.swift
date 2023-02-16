ac68918b0873615d8668d640725c4b35e25afa75
Luis Vienrich <a20172501@pucp.edu.pe>
2023-02-15 23:23:19 -0500

    Update v2.4

--- MyCrypto/MyCrypto/Core/ViewModel/HomeViewModel.swift
+++ MyCrypto/MyCrypto/Core/ViewModel/HomeViewModel.swift
@@ -23,11 +23,28 @@ class HomeViewModel: ObservableObject {
     
     func AddSubs() {
         
-        dataService.$allCoins
-            .sink { [weak self] returnedCoin in
-                self?.allCoins = returnedCoin
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map({ (text, startingCoins) -> [CoinModel] in
+                
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                
+                let lowercasedText = text.lowercased()
+                return startingCoins.filter { (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+            })
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
             }
             .store(in: &cancel)
+            
     }
    
 }
